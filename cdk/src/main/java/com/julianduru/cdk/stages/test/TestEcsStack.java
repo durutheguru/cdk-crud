2b8820630924b70fcd07718cbd2c9fac436eb652
Julian Duru <durutheguru@gmail.com>
2023-12-04 21:37:07 +0100

    - switch to port 80

--- cdk/src/main/java/com/julianduru/cdk/stages/test/TestEcsStack.java
+++ cdk/src/main/java/com/julianduru/cdk/stages/test/TestEcsStack.java
@@ -5,6 +5,7 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
 import software.constructs.Construct;
 
@@ -27,46 +28,118 @@ public class TestEcsStack extends Stack {
     public TestEcsStack(final Construct scope, final String id, final StackProps props, final Map<String, String> variableMap) {
         super(scope, id, props);
 
-//        Cluster cluster = new Cluster(this, Main.prefixApp("ECSCluster"));
+        setup();
+    }
 
-        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, "ECSTaskDefinition")
-                .build();
-        taskDefinition.addContainer(
-            "crud-container",
-            ContainerDefinitionOptions.builder()
-                .image(ContainerImage.fromRegistry("public.ecr.aws/h9p9w3g7/crud:0.1.0-alpha.81"))
-                .portMappings(
-                    List.of(
-                        PortMapping.builder()
-                            .containerPort(8080)
-                            .build()
-                    )
-                )
-                .logging(
-                    LogDriver.awsLogs(
-                        AwsLogDriverProps.builder()
-                            .streamPrefix("str-prfix")
-                            .build()
-                    )
-                )
-                .build()
-        );
 
+    private void setup() {
         service = ApplicationLoadBalancedFargateService.Builder
             .create(this, Main.prefixApp("ECSFargateService"))
-//            .cluster(cluster)
-            .taskDefinition(taskDefinition)
+            .taskImageOptions(
+                ApplicationLoadBalancedTaskImageOptions.builder()
+                    .image(
+                        ContainerImage.fromRegistry("public.ecr.aws/h9p9w3g7/crud:0.1.0-alpha.84")
+                    )
+                    .enableLogging(true)
+                    .logDriver(
+                        LogDriver.awsLogs(
+                            AwsLogDriverProps.builder()
+                                .streamPrefix("str-prfix")
+                                .build()
+                        )
+                    )
+                    .build()
+            )
             .publicLoadBalancer(true)
-//            .lis
+            .assignPublicIp(true)
             .build();
 
-        service.getTargetGroup().configureHealthCheck(
-            HealthCheck.builder()
-                .path("/actuator/health")
-                .port("8080")
-                .build()
-        );
     }
+
+
+//    private void setup() {
+//        service = ApplicationLoadBalancedFargateService.Builder
+//            .create(this, Main.prefixApp("ECSFargateService"))
+//            .taskImageOptions(
+//                ApplicationLoadBalancedTaskImageOptions.builder()
+//                    .image(
+//                        ContainerImage.fromRegistry("public.ecr.aws/h9p9w3g7/crud:0.1.0-alpha.84")
+//                    )
+//                    .containerPort(8080)
+//                    .enableLogging(true)
+//                    .logDriver(
+//                        LogDriver.awsLogs(
+//                            AwsLogDriverProps.builder()
+//                                .streamPrefix("str-prfix")
+//                                .build()
+//                        )
+//                    )
+//                    .build()
+//            )
+//            .publicLoadBalancer(true)
+//            .assignPublicIp(true)
+//            .build();
+//
+//    }
+
+
+//    private void setup() {
+//
+////        Cluster cluster = new Cluster(this, Main.prefixApp("ECSCluster"));
+//
+////        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, "ECSTaskDefinition")
+////                .build();
+////
+////        taskDefinition.addContainer(
+////            "crud-container",
+////            ContainerDefinitionOptions.builder()
+////                .image(ContainerImage.fromRegistry("public.ecr.aws/h9p9w3g7/crud:0.1.0-alpha.84"))
+////                .portMappings(
+////                    List.of(
+////                        PortMapping.builder()
+////                            .containerPort(8080)
+////                            .build()
+////                    )
+////                )
+////                .logging(
+////                    LogDriver.awsLogs(
+////                        AwsLogDriverProps.builder()
+////                            .streamPrefix("str-prfix")
+////                            .build()
+////                    )
+////                )
+////                .build()
+////        );
+//
+//        service = ApplicationLoadBalancedFargateService.Builder
+//            .create(this, Main.prefixApp("ECSFargateService"))
+//            .taskImageOptions(
+//                ApplicationLoadBalancedTaskImageOptions.builder()
+//                    .image(
+//                        ContainerImage.fromRegistry("public.ecr.aws/h9p9w3g7/crud:0.1.0-alpha.84")
+//                    )
+//                    .containerPort(8080)
+//                    .enableLogging(true)
+//                    .logDriver(
+//                        LogDriver.awsLogs(
+//                            AwsLogDriverProps.builder()
+//                                .streamPrefix("str-prfix")
+//                                .build()
+//                        )
+//                    )
+//                    .build()
+//            )
+//            .publicLoadBalancer(true)
+//            .assignPublicIp(true)
+//            .build();
+//
+////        service.getTargetGroup().configureHealthCheck(
+////            HealthCheck.builder()
+////                .path("/actuator/health")
+////                .port("8080")
+////                .build()
+////        );
+//    }
 
 
     public ApplicationLoadBalancedFargateService getApplicationLoadBalancedFargateService() {
@@ -75,3 +148,4 @@ public class TestEcsStack extends Stack {
 
 
 }
+
